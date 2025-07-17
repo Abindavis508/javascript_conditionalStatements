570fe8f71c4769de1a93a2938cb94cb06b3a0a24
Abindavis508 <abindavis18@gmail.com>
2025-07-17 19:25:06 +0530

    files modified

---
+++ qs8.js
@@ -0,0 +1,42 @@
+let numberOfMOnth = 12;
+switch (numberOfMOnth) {
+    case 1:
+        console.log("january consists of 31 days");
+        break;
+    case 2:
+        console.log("february consists of 28 days");
+        break;
+    case 3:
+        console.log("march consists of 31 days");
+        break;
+    case 4:
+        console.log("april consists of 30 days");
+        break;
+    case 5:
+        console.log("may consists of 31 days");
+        break;
+    case 6:
+        console.log("june consists of 30 days");
+        break;
+    case 7:
+        console.log("july consists of 31 days");
+        break;
+    case 8:
+        console.log("august consists of 31 days");
+        break;
+    case 9:
+        console.log("september consists of 30 days");
+        break;
+    case 10:
+        console.log("october consists of 31 days");
+        break;
+    case 11:
+        console.log("november consists of 30 days");
+        break;
+    case 12:
+        console.log("december consists of 31 days");
+        break;
+    default:
+        console.log("invalid month number");
+
+}
