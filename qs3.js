e57bb81b76bd070f7be4543bb1fdd0c927405d79
Abindavis508 <abindavis18@gmail.com>
2025-07-17 19:06:26 +0530

    files created and modified

---
+++ qs3.js
@@ -0,0 +1,11 @@
+let age=90;
+if(age<=13){
+    console.log("you are a child");
+}else if(age<=19){
+    console.log("you are a teenageer");
+}else if(age<=60){
+    console.log("you are a adult");
+}
+else if(age>=61){
+    console.log("you are a senior");
+}
