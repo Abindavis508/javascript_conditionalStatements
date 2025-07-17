8c425e052ab99c8ae89529e1f79442af6ae070d5
Abindavis508 <abindavis18@gmail.com>
2025-07-17 19:54:37 +0530

    files modified

---
+++ qs1.js
@@ -0,0 +1,8 @@
+let num=0;
+if (num===0){
+    console.log("the number is zero");
+}else if(num<0){
+    console.log("the number is negative");
+}else if(num>0){
+    console.log("the number is positive");
+}
