f69be963d15c61b9a98906a77b5c3a4decc13399
Abindavis508 <abindavis18@gmail.com>
2025-07-17 19:46:27 +0530

    added

---
+++ qs5.js
@@ -0,0 +1,14 @@
+let num=104;
+ if (num % 2 === 0) {
+    if (num > 100) {
+      console.log("Even and greater than 100");
+    } else {
+      console.log("Even and less than or equal to 100");
+    }
+  } else { 
+    if (num > 100) {
+      console.log("Odd and greater than 100");
+    } else {
+      console.log("Odd and less than or equal to 100");
+    }
+  }
