b818a2b7f27aae07ef3d44f931c7cfa218562ece
Abindavis508 <abindavis18@gmail.com>
2025-07-17 20:25:04 +0530

    files created and modified

---
+++ qs6.js
@@ -0,0 +1,13 @@
+let username="admin";
+let password=1234;
+
+if(username==="admin"){
+    
+    if (password===1234){
+        console.log("access granted");
+    }else {
+        console.log("wrong password");
+    }
+}else {
+    console.log("unknown user");
+}
