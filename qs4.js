0a2f819daae72db17dffc87c66430cc657b54607
Abindavis508 <abindavis18@gmail.com>
2025-07-17 19:13:25 +0530

    files added

---
+++ qs4.js
@@ -0,0 +1,19 @@
+let vehicletype = "b";
+switch (vehicletype) {
+    case "c":
+        console.log("you have a car");
+        break;
+    case "b":
+        console.log("you have a bike");
+        break;
+    case "t":
+        console.log("you have a truck");
+        break;
+    case "a":
+        console.log("you have a auto");
+        break;
+        default:
+            console.log("Unknown vehicle");
+            break;
+
+}
